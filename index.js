ede3892be286390affc82a38ad4b53fddf3307d9
RubbST <115050247+RubbST@users.noreply.github.com>
2022-10-11 16:23:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+let isModalOpen = false;
+let contrastToggle = true;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+        document.getElementById("light-icon").style.display = "none";
+        document.getElementById("dark-icon").style.display = "block";
+    } else {
+        document.body.classList.remove("dark-theme")
+        document.getElementById("light-icon").style.display = "block";
+        document.getElementById("dark-icon").style.display = "none";
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible'
+
+
+    emailjs
+        .sendForm(
+            'service_phyrs7u',
+            'template_2z407cw',
+            event.target,
+            'qPTmIfiEmStyhe0AE'
+        ).then(() => {
+            loading.classList.remove('modal__overlay--visible')
+            success.classList += ' modal__overlay--visible'
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible')
+            alert(
+                "The email service is temporarily unavailible. Please contact me directly on thomasjprice2@gmail.com"
+            )
+        })
+
+}
+
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
